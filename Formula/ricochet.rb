19c0c0f14d57f0689afe0835e2a490f7fae8523d
pat-s <patrick.schratz@gmail.com>
2025-09-18 15:57:33 +0200

    init

---
+++ Formula/ricochet.rb
@@ -0,0 +1,17 @@
+class Ricochet < Formula
+  desc "ricochet - Put R & Julia in production"
+  homepage "https://github.com/ricochet-rs/cli"
+  license "GPL-3.0-or-later"
+  head "https://github.com/ricochet-rs/cli.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "ricochet", shell_output("#{bin}/ricochet --help")
+    system "#{bin}/ricochet", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# homebrew-tap
+
+## Installation
+
+Add the tap:
+
+```shell
+brew tap ricochet-rs/tap
+```
+
+Install formula:
+
+```shell
+brew install <formula> # e.g. brew install ricochet
+```
+
+## Update formulas
+
+```shell
+# make sure you installed the formula first, then:
+brew bump-formula-pr -v --strict --version <new version> ricochet-rs
+```
