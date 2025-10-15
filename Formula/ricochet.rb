a1a6022f4c6c5f6e36e08a96697e830b933f7ea3
pat-s <patrick.schratz@gmail.com>
2025-10-16 01:24:59 +0200

    ci: add update-formula workflow

--- Formula/ricochet.rb
+++ Formula/ricochet.rb
@@ -2,7 +2,7 @@ class Ricochet < Formula
   desc "Put R & Julia in production"
   homepage "https://github.com/ricochet-rs/cli"
   url "https://github.com/ricochet-rs/cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "eca300b14dc4541301718d500aae6b03958e367407b7e6d4f61e2f5dba097e78"
+  sha256 "6a1cb7ead8f2ee0f076c8afc3307cde3d878f206d9aa1fcbffa6d57bfa65f52d"
   license "AGPL-3.0-or-later"
   head "https://github.com/ricochet-rs/cli.git", branch: "main"
 
