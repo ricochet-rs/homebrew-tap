ea9b45a9718e78e8f5ba4b1e7b9b43c36c69d60e
pat-s <patrick.schratz@gmail.com>
2025-10-16 00:50:52 +0200

    chore: add bottle block

--- Formula/ricochet.rb
+++ Formula/ricochet.rb
@@ -6,6 +6,11 @@ class Ricochet < Formula
   license "AGPL-3.0-or-later"
   head "https://github.com/ricochet-rs/cli.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/ricochet-rs/homebrew-tap/releases/download/v0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e20a395e1010a85eee2f6d9b9ffc9d590d5067b8ba00b85c590a589b081277e"
+  end
+
   depends_on "rust" => :build
 
   def install
