18df3472b429874368f5babf4d90e174273e9314
pat-s <patrick.schratz@gmail.com>
2026-03-02 02:16:50 +0100

    fix: remove stale bottle block for v0.4.0
    The bottle SHAs were from v0.3.0 and pointed to a release that doesn't exist.
    Removing so publish.yml can rebuild and re-add them.

--- Formula/ricochet.rb
+++ Formula/ricochet.rb
@@ -13,14 +13,6 @@ class Ricochet < Formula
         using: :git
   end
 
-  bottle do
-    root_url "https://github.com/ricochet-rs/homebrew-tap/releases/download/v0.4.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a24e16737f75a4d75376ebd29899f6907bccc7ca05fdb1b2232c3c5e3deea451"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "992dbc097d310b96581d1ef219744858af633e8a32f159debd5eda03c1b50fa3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23644ecda8a8c5448a521b327fb80fb4fb3a6b72b568a4c47062db68657265d7"
-    sha256 cellar: :any_skip_relocation, sequoia:       "c90f6803aca1369ab27f46e70e57fc824cec26a465ccce8c42ca28f71932851a"
-  end
-
   depends_on "rust" => :build
 
   # Pass through environment for git auth (private dependencies)
